bc284222a5b2b2243ece141ddd70fa2da4d561b8
Thang <phamdinhthang1412@gmail.com>
2019-11-29 10:10:09 +0700

    update css

--- wp-content/themes/setsail-child/functions.php
+++ wp-content/themes/setsail-child/functions.php
@@ -17,7 +17,8 @@ if ( ! function_exists('setsail_select_child_theme_enqueue_scripts')) {
 function my_styles_method()
 {
     $background_image = setsail_select_options()->getOptionValue('footer_background_container');
-    $image_pattern    = get_stylesheet_directory_uri() . '/assets/image/pattern.png';
+    $image_pattern_toong    = get_stylesheet_directory_uri() . '/assets/image/toong_pattern.png';
+    $image_pattern_toong2    = get_stylesheet_directory_uri() . '/assets/image/pattern.png';
     $image_toong      = get_stylesheet_directory_uri() . '/assets/image/background.png';
     $custom_css       = "
     .qodef-tours-type-toong .qodef-tours-gim-holder-inner{
@@ -29,7 +30,16 @@ function my_styles_method()
     left: 0;
     width: 100%;
     height: 100%;
-    background: url({$image_pattern }) center/100% 100% no-repeat;
+    background: url({$image_pattern_toong2 }) center/100% 100% no-repeat;
+    opacity: 1;
+}
+.qodef-tours-dwt-holder .qodef-td-items.destination .qodef-tdi-image:after {
+    content: \"\";
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url({$image_pattern_toong }) center/100% 100% no-repeat;
     opacity: 1;
 }
                 .qodef-footer-top-holder{
