c66b50114f07783128f405d3527388407bdff227
Thang <phamdinhthang1412@gmail.com>
2019-12-30 14:07:29 +0700

    update bug loadmore signature tour

--- wp-content/plugins/setsail-tours/post-types/destinations/shortcodes/destination-with-tours/js/des-with-tour.js
+++ wp-content/plugins/setsail-tours/post-types/destinations/shortcodes/destination-with-tours/js/des-with-tour.js
@@ -1,23 +1,31 @@
-jQuery(document).ready(function($){
-    let inside = $('.inside-circle');
-    $.each(inside,function(index,value){
-        let grading = $(this).data('value');
-        let percent= grading/5*100;
-        percent=percent*180/100;
+jQuery(document).ready(function ($) {
 
-        $(this).html(grading);
-        /*.myLoading-indicator-circle-wrap .mask.full,
-.myLoading-indicator-circle-wrap .fill*/
-        var parent=$(this).parents('.myLoading-indicator-circle-wrap');
-        rotate(parent.find('.mask.full'),percent);
-        rotate(parent.find('.fill'),percent);
-    })
+    window.grading_function = function () {
+        let inside = $(document).find('.inside-circle');
+        console.log(inside);
+        if (inside) {
+            $.each(inside, function (index, value) {
+                let grading = $(this).data('value');
+                let percent = grading / 5 * 100;
+                percent = percent * 180 / 100;
 
-    function rotate(el,degree) {
+                $(this).html(grading);
+                /*.myLoading-indicator-circle-wrap .mask.full,
+        .myLoading-indicator-circle-wrap .fill*/
+                var parent = $(this).parents('.myLoading-indicator-circle-wrap');
+                rotate(parent.find('.mask.full'), percent);
+                rotate(parent.find('.fill'), percent);
+            })
+        }
+        console.log('inside');
+    }
+    window.grading_function();
+
+    function rotate(el, degree) {
         // For webkit browsers: e.g. Chrome
-        el.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
+        el.css({WebkitTransform: 'rotate(' + degree + 'deg)'});
         // For Mozilla browser: e.g. Firefox
-        el.css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
+        el.css({'-moz-transform': 'rotate(' + degree + 'deg)'});
     }
 
 });
