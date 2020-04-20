dfae6ca3bb4fd14108f35651203056335a6d940f
thang <thangpd.slz@gmail.com>
2020-04-20 16:37:04 +0700

    add shortcode hive gallery

---
+++ wp-content/plugins/setsail-core/shortcodes/hivegallery/hivegallery-tab.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SetSailCore\CPT\Shortcodes\AccordionTab;
+
+use SetSailCore\Lib;
+
+class AccordionTab implements Lib\ShortcodeInterface {
+	private $base;
+	
+	function __construct() {
+		$this->base = 'qodef_accordion_tab';
+		add_action( 'vc_before_init', array( $this, 'vcMap' ) );
+	}
+	
+	public function getBase() {
+		return $this->base;
+	}
+	
+	public function vcMap() {
+		if ( function_exists( 'vc_map' ) ) {
+			vc_map(
+				array(
+					"name"                    => esc_html__( 'Accordion Tab', 'setsail-core' ),
+					"base"                    => $this->base,
+					"as_child"                => array( 'only' => 'qodef_accordion' ),
+					'is_container'            => true,
+					"category"                => esc_html__( 'by SETSAIL', 'setsail-core' ),
+					"icon"                    => "icon-wpb-accordion-tab extended-custom-icon",
+					"show_settings_on_create" => true,
+					"js_view"                 => 'VcColumnView',
+					"params"                  => array(
+						array(
+							'type'        => 'textfield',
+							'param_name'  => 'title',
+							'heading'     => esc_html__( 'Title', 'setsail-core' ),
+							'description' => esc_html__( 'Enter accordion section title', 'setsail-core' )
+						),
+						array(
+							'type'       => 'dropdown',
+							'param_name' => 'title_tag',
+							'heading'    => esc_html__( 'Title Tag', 'setsail-core' ),
+							'value'      => array_flip( setsail_select_get_title_tag( true, array( 'p' => 'p' ) ) ),
+						)
+					)
+				)
+			);
+		}
+	}
+	
+	public function render( $atts, $content = null ) {
+		$default_atts = array(
+			'title'     => 'Section',
+			'title_tag' => 'h6'
+		);
+		$params       = shortcode_atts( $default_atts, $atts );
+		
+		$params['content']   = $content;
+		$params['title_tag'] = ! empty( $params['title_tag'] ) ? $params['title_tag'] : $default_atts['title_tag'];
+		
+		$output = setsail_core_get_shortcode_module_template_part( 'templates/accordion-template', 'accordions', '', $params );
+		
+		return $output;
+	}
+}
