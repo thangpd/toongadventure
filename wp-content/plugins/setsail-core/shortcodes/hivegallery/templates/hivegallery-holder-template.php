1834f169c02f23168ca67581afb98e77788b3979
thang <thangpd.slz@gmail.com>
2020-05-09 16:21:59 +0700

    update hive gallery

--- wp-content/plugins/setsail-core/shortcodes/hivegallery/templates/hivegallery-holder-template.php
+++ wp-content/plugins/setsail-core/shortcodes/hivegallery/templates/hivegallery-holder-template.php
@@ -1,58 +1,50 @@
+<?php
+if (!empty($params['image_list'])) {
+    $format = '<div class="grid grid-1">%1$s</div><div class="grid grid-2">%2$s</div>';
+    $format_item_1 = '    <div class="block-inner">
+                    <div class="title">
+                        %1$s
+                    </div>
+                    <div class="block">
+                        <img src="%2$s"
+                             alt="">
+                    </div>
+                </div>
+            ';
+    $format_item_2 = '    <div class="block-inner">
+                    <div class="block">
+                        <img src="%2$s"
+                             alt="">
+                    </div>
+                    <div class="title">
+                        %1$s
+                    </div>
+                </div>
+            ';
+    $image_list = vc_param_group_parse_atts($params['image_list']);
+    $count = 1;
+    $str_item = [];
+    foreach ($image_list as $item) {
+        $image = wp_get_attachment_image_src($item['image'], 'large');
+        if ($count <= count($image_list) / 2) {
+            echo '<pre>';
+            print_r(count($image_list) / 2);
+            echo '</pre>';
+            $str_item['grid1'] .= sprintf($format_item_1, $item['text'], $image[0]);
+        } else {
+            $str_item['grid2'] .= sprintf($format_item_2, $item['text'], $image[0]);
+        }
+        $count++;
+    }
+    $print = sprintf($format, $str_item['grid1'], $str_item['grid2']);
+}
+
+?>
+
 <div class="qodef-accordion-holder <?php echo esc_attr($holder_classes); ?> clearfix">
-    <h2 class="experience-title">
-        Trải nghiệm độc nhất
-    </h2>
     <div class="experience-wrapper">
         <div class="hive-grid">
-            <div class="grid grid-1">
-                <div class="block-inner">
-                    <div class="title">
-                        Khám phá hồ Cô Đơn giữa rừng
-                    </div>
-                    <div class="block">
-                        <img src="https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-                             alt="">
-                    </div>
-                </div>
-                <div class="block-inner">
-                    <div class="title">
-                        Check-in mũi Đá Vách được xem là Ba Li Việt Nam
-                    </div>
-                    <div class="block">
-                        <img src="https://images.unsplash.com/photo-1544568100-847a948585b9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-                             alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="grid grid-2">
-                <div class="block-inner">
-                    <div class="block">
-                        <img src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-                             alt="">
-                    </div>
-                    <div class="title">
-                        Trải nghiệm xe mui trần băng qua những bãi biển đẹp nhất Ninh Thuận
-                    </div>
-                </div>
-                <div class="block-inner">
-                    <div class="block">
-                        <img src="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-                             alt="">
-                    </div>
-                    <div class="title">
-                        Bãi cắm trại riêng biệt, tiện nghi tại bãi Chà Là hoang sơ
-                    </div>
-                </div>
-                <div class="block-inner">
-                    <div class="block">
-                        <img src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-                             alt="">
-                    </div>
-                    <div class="title">
-                        Lặn ngắm san hô và hệ sinh thái biển
-                    </div>
-                </div>
-            </div>
+            <?php echo $print; ?>
         </div>
     </div>
 </div>
