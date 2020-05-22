2dc8467defec15d48a423090cb7105c726be7870
Thomas Pham <tom@ThangDebian.thangpd.site>
2020-05-23 02:04:49 +0700

    Update team css responsive

--- wp-content/plugins/setsail-core/shortcodes/shortcode-team/shortcode-team.php
+++ wp-content/plugins/setsail-core/shortcodes/shortcode-team/shortcode-team.php
@@ -1,96 +1,105 @@
 <?php
+
 namespace SetSailCore\CPT\Shortcodes\ShortcodeTeam;
 
 use SetSailCore\Lib;
 
 class ShortcodeTeam implements Lib\ShortcodeInterface {
 	private $base;
-	
+
 	function __construct() {
 		$this->base = 'qodef_shortcode_team';
 		add_action( 'vc_before_init', array( $this, 'vcMap' ) );
 	}
-	
+
 	public function getBase() {
 		return $this->base;
 	}
-	
+
 	public function vcMap() {
 		if ( function_exists( 'vc_map' ) ) {
 			vc_map(
 				array(
-					'name'                    => esc_html__( 'Shortcode Team', 'setsail-core' ),
-					'base'                    => $this->base,
-					'content_element'         => true,
-					'category'                => esc_html__( 'by SETSAIL', 'setsail-core' ),
-					'icon'                    => 'icon-wpb-shortcode_team extended-custom-icon',
-					'show_settings_on_create' => true,
-					'js_view'                 => 'VcColumnView',
-					'params'                  => array(
+					'name'     => esc_html__( 'Shortcode Team', 'setsail-core' ),
+					'base'     => $this->base,
+					'category' => esc_html__( 'by SETSAIL', 'setsail-core' ),
+					'params'   => array(
 						array(
-							'type'        => 'attach_image',
-							'param_name'  => 'user_attach_image',
-							'heading'     => esc_html__( 'Image', 'setsail-core' ),
+							'type'       => 'dropdown',
+							'param_name' => 'style',
+							'heading'    => esc_html__( 'Style', 'setsail-core' ),
+							'value'      => array(
+								esc_html__( 'Style 1', 'setsail-core' ) => 'style-1',
+								esc_html__( 'Style 2', 'setsail-core' ) => 'style-2',
+								esc_html__( 'Style 3', 'setsail-core' ) => 'style-3',
+							),
 						),
 						array(
-							'type'        => 'textfield',
-							'param_name'  => 'user_job',
-							'heading'     => esc_html__( 'Công việc', 'setsail-core' ),
+							'type'       => 'attach_image',
+							'param_name' => 'user_attach_image',
+							'heading'    => esc_html__( 'Image', 'setsail-core' ),
 						),
 						array(
-							'type'        => 'textfield',
-							'param_name'  => 'user_name',
-							'heading'     => esc_html__( 'Tên người', 'setsail-core' ),
+							'type'       => 'textfield',
+							'param_name' => 'user_job',
+							'heading'    => esc_html__( 'Công việc', 'setsail-core' ),
 						),
 						array(
-							'type'        => 'textarea',
-							'param_name'  => 'user_des',
-							'heading'     => esc_html__( 'Giới thiệu', 'setsail-core' ),
+							'type'       => 'textfield',
+							'param_name' => 'user_name',
+							'heading'    => esc_html__( 'Tên người', 'setsail-core' ),
 						),
-						
-						
+						array(
+							'type'       => 'textarea',
+							'param_name' => 'user_des',
+							'heading'    => esc_html__( 'Giới thiệu', 'setsail-core' ),
+						),
+
+
 					)
 				)
 			);
 		}
 	}
 
-	public function enqueue_scripts()
-    {
-        wp_enqueue_script('shortcode_team_js');
-        wp_enqueue_style('shortcode_team_css');
-    }
-	
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'shortcode_team_js' );
+		wp_enqueue_style( 'shortcode_team_css' );
+	}
+
 	public function render( $atts, $content = null ) {
-        $this->enqueue_scripts();
+		$this->enqueue_scripts();
 		$default_atts = array(
-			'user_attach_image'           => '',
-			'user_job'           => '',
-			'user_name'           => '',
-			'user_des'           => '',
+			'user_attach_image' => '',
+			'user_job'          => '',
+			'user_name'         => '',
+			'user_des'          => '',
+			'style'             => 'style-1',
 		);
 		$params       = shortcode_atts( $default_atts, $atts );
-		
-		$params['user_attach_image'] 	= ! empty( $params['user_attach_image'] ) ? $params['user_attach_image'] : '';
-		$params['user_job'] 	= ! empty( $params['user_job'] ) ? $params['user_job'] : '';
-		$params['user_name'] 	= ! empty( $params['user_name'] ) ? $params['user_name'] : '';
-		$params['user_des'] 	= ! empty( $params['user_des'] ) ? $params['user_des'] : '';
-		
 
-		
-		$output = setsail_core_get_shortcode_module_template_part( 'templates/shortcode-team-holder-template', 'shortcode-team', '', $params );
-		
+		$params['user_attach_image'] = ! empty( $params['user_attach_image'] ) ? $params['user_attach_image'] : '';
+		$params['user_job']          = ! empty( $params['user_job'] ) ? $params['user_job'] : '';
+		$params['user_name']         = ! empty( $params['user_name'] ) ? $params['user_name'] : '';
+		$params['user_des']          = ! empty( $params['user_des'] ) ? $params['user_des'] : '';
+		$params['hoder_class']       = $this->getHolderClasses( $params );
+		if ( $params['style'] == 'style-1' ) {
+			$output = setsail_core_get_shortcode_module_template_part( 'templates/shortcode-team-holder-template', 'shortcode-team', '', $params );
+		} else {
+			$output = setsail_core_get_shortcode_module_template_part( 'templates/shortcode-team-holder-template-2', 'shortcode-team', '', $params );
+		}
+
 		return $output;
 	}
-	
+
 	private function getHolderClasses( $params ) {
 		$holder_classes = array( 'qodef-ac-default' );
-		
+
 		$holder_classes[] = ! empty( $params['custom_class'] ) ? esc_attr( $params['custom_class'] ) : '';
 		$holder_classes[] = $params['style'] == 'toggle' ? 'qodef-toggle' : 'qodef-shortcode_team';
 		$holder_classes[] = ! empty( $params['layout'] ) ? 'qodef-ac-' . esc_attr( $params['layout'] ) : '';
 		$holder_classes[] = ! empty( $params['background_skin'] ) ? 'qodef-' . esc_attr( $params['background_skin'] ) . '-skin' : '';
-		
+
 		return implode( ' ', $holder_classes );
 	}
 }
--- wp-content/plugins/setsail-core/shortcodes/shortcode-team/templates/shortcode-team-holder-template-2.php
+++ wp-content/plugins/setsail-core/shortcodes/shortcode-team/templates/shortcode-team-holder-template-2.php
@@ -1,7 +1,6 @@
-
 <div class="qodef-shortcode_team-holder <?php echo esc_attr( $holder_classes ); ?> clearfix">
     <div class="shortcode-team-user <?php echo $style; ?>">
-
+        <div class="holder-col-1">
             <div class="shortcode-team-user-image ">
 				<?php echo wp_get_attachment_image( esc_attr( $user_attach_image ), 'large' ); ?>
             </div>
@@ -9,9 +8,12 @@
                 <p class="hortcode-team-user-info-job "><?php echo esc_attr( $user_job ); ?></p>
                 <p class="hortcode-team-user-info-name"><?php echo esc_attr( $user_name ); ?></p>
             </div>
+        </div>
+        <div class="holder-col-2">
             <div class="hortcode-team-user-des">
-					<?= __( $user_des ) ?>
+				<?= __( $user_des ) ?>
             </div>
+        </div>
     </div>
 </div>
 
