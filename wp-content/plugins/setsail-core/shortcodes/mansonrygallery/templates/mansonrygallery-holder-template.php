ee6531dd6fa5790d19e78819f33327a7457dc47d
Tam Nguyen <tam.nguyen@365dep.vn>
2020-06-03 00:10:06 +0700

    update css

--- wp-content/plugins/setsail-core/shortcodes/mansonrygallery/templates/mansonrygallery-holder-template.php
+++ wp-content/plugins/setsail-core/shortcodes/mansonrygallery/templates/mansonrygallery-holder-template.php
@@ -3,16 +3,16 @@
         Bộ sưu tập
     </div>
     <div class="vc_row wpb_row vc_inner vc_row-fluid qodef-grid-no-gutter">
-        <div class="wpb_column vc_column_container vc_col-sm-12 banner-full">
+        <div class="wpb_column vc_column_container vc_col-xs-12 banner-full">
             <a href="#"><img class="w-100" src="<?php echo plugin_dir_url(__DIR__); ?>/assets/img/gallery-img-full.png" alt="img"></a>
         </div>
-        <div class="wpb_column vc_column_container vc_col-sm-4 banner-full">
+        <div class="wpb_column vc_column_container vc_col-xs-4 banner-full">
             <a href="#"><img class="w-100" src="<?php echo plugin_dir_url(__DIR__); ?>/assets/img/gallery-img2.jpg" alt="img"></a>
         </div>
-        <div class="wpb_column vc_column_container vc_col-sm-4 banner-full">
+        <div class="wpb_column vc_column_container vc_col-xs-4 banner-full">
             <a href="#"><img class="w-100" src="<?php echo plugin_dir_url(__DIR__); ?>/assets/img/gallery-img3.jpg" alt="img"></a>
         </div>
-        <div class="wpb_column vc_column_container vc_col-sm-4 banner-full">
+        <div class="wpb_column vc_column_container vc_col-xs-4 banner-full">
             <a href="#"><img class="w-100" src="<?php echo plugin_dir_url(__DIR__); ?>/assets/img/gallery-img4.jpg" alt="img"></a>
         </div>
     </div>
