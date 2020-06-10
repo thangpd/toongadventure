3dacd1de60bdda6a8b657935a1bcdaeffc9e893f
thomas <thangpd.slz@gmail.com>
2020-06-10 00:43:03 -0400

    delete setsail-tourddd

--- wp-content/plugins/setsail-toursdddd/const.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('SETSAIL_TOURS_VERSION', '1.0.1');
-define('SETSAIL_TOURS_ABS_PATH', dirname(__FILE__));
-define('SETSAIL_TOURS_REL_PATH', dirname(plugin_basename(__FILE__)));
-define('SETSAIL_TOURS_URL_PATH', plugin_dir_url( __FILE__ ));
-define('SETSAIL_TOURS_CPT_PATH', SETSAIL_TOURS_ABS_PATH.'/post-types');
-define('SETSAIL_TOURS_ASSETS_PATH', SETSAIL_TOURS_ABS_PATH.'/assets');
-define('SETSAIL_TOURS_ASSETS_URL_PATH', SETSAIL_TOURS_URL_PATH.'assets');
