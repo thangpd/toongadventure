cb4a3607c23a2793adf28b0e76d1772ac4db018a
thomas <thangpd.slz@gmail.com>
2020-06-12 23:05:30 -0400

    update shortcode team template 2

--- wp-content/plugins/setsail-core/shortcodes/shortcode-team/templates/shortcode-team-holder-template-2.php
+++ wp-content/plugins/setsail-core/shortcodes/shortcode-team/templates/shortcode-team-holder-template-2.php
@@ -4,13 +4,13 @@
             <div class="shortcode-team-user-image ">
 				<?php echo wp_get_attachment_image( esc_attr( $user_attach_image ), 'large' ); ?>
             </div>
-            <div class="hortcode-team-user-info text-uppercase">
-                <p class="hortcode-team-user-info-job "><?php echo esc_attr( $user_job ); ?></p>
-                <p class="hortcode-team-user-info-name"><?php echo esc_attr( $user_name ); ?></p>
+            <div class="shortcode-team-user-info text-uppercase">
+                <p class="shortcode-team-user-info-job "><?php echo esc_attr( $user_job ); ?></p>
+                <p class="shortcode-team-user-info-name"><?php echo esc_attr( $user_name ); ?></p>
             </div>
         </div>
         <div class="holder-col-2">
-            <div class="hortcode-team-user-des">
+            <div class="shortcode-team-user-des">
 				<?= __( $user_des ) ?>
             </div>
         </div>
